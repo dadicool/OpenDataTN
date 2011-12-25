a7bb234329ded64912b79997b4ddd407f805c04e
Dali Kilani <dali.kilani@gmail.com>
2011-12-24 17:48:57 -0800

    Adding aggregation tests (Still need to make sure that the reference data is correct though ...)

--- tnac_api/test/tnac-aggregation-test.js
+++ tnac_api/test/tnac-aggregation-test.js
@@ -3,7 +3,9 @@ var assert = require('assert');
 var APIeasy = require('api-easy');
 var config = require('./config.js');
 var expectedData = require('./fixtures/tnac_aggreg_centre.js');
-var expectedResult = expectedData.result;
+var expectedResultCentre = expectedData.resultCentre;
+var expectedResultDelegation = expectedData.resultDelegation;
+var expectedResultCirconscription = expectedData.resultCirconscription;
 var centre_test_id = expectedData.centre_test_id; 
 var deleg_test_id = expectedData.deleg_test_id; 
 var circ_test_id = expectedData.circ_test_id; 
@@ -40,6 +42,66 @@ suite.use(config.host, config.port)
         assert.deepEqual(sorted_listes, listes);
     })
     .expect('should provide the expected response', function (err, res, body) {
-        assert.deepEqual(expectedResult, JSON.parse(body));
+        assert.deepEqual(expectedResultCentre, JSON.parse(body));
     })
+    .undiscuss()
+    .unpath()
+    .discuss('When asking for the aggregated results for a specific delegation')
+    .path('/tnac/v1/vote')
+    .get('/'+ circ_test_id + '/' + deleg_test_id )
+    .expect(200)
+    .expect('should respond with exactly one delegation', function (err, res, body) {
+        assert.isObject(JSON.parse(body));
+    })
+    .expect('should have a "resultat" with at least votes for one list ', function (err, res, body) {
+        var result = JSON.parse(body);
+        assert.isDefined(result.resultat);
+        assert.isArray(result.resultat.listes);
+        assert.isTrue(result.resultat.listes.length >= 1);
+    })
+    .expect('should return the votes for the list in descending order', function (err, res, body) {
+        var result = JSON.parse(body);
+        assert.isDefined(result.resultat);
+        assert.isArray(result.resultat.listes);
+        assert.isTrue(result.resultat.listes.length >= 1);
+        var listes = result.resultat.listes;
+        var sorted_listes = listes.sort(function (a,b) {
+                                            return b.vote - a.vote;
+                                          }
+                                       );
+        assert.deepEqual(sorted_listes, listes);
+    })
+    .expect('should provide the expected response', function (err, res, body) {
+        assert.deepEqual(expectedResultDelegation, JSON.parse(body));
+    })
+//    .undiscuss()
+//    .unpath()
+//    .discuss('When asking for the aggregated results for a specific circonscription')
+//    .path('/tnac/v1/vote')
+//    .get('/'+ circ_test_id )
+//    .expect(200)
+//    .expect('should respond with exactly one circonscription', function (err, res, body) {
+//        assert.isObject(JSON.parse(body));
+//    })
+//    .expect('should have a "resultat" with at least votes for one list ', function (err, res, body) {
+//        var result = JSON.parse(body);
+//        assert.isDefined(result.resultat);
+//        assert.isArray(result.resultat.listes);
+//        assert.isTrue(result.resultat.listes.length >= 1);
+//    })
+//    .expect('should return the votes for the list in descending order', function (err, res, body) {
+//        var result = JSON.parse(body);
+//        assert.isDefined(result.resultat);
+//        assert.isArray(result.resultat.listes);
+//        assert.isTrue(result.resultat.listes.length >= 1);
+//        var listes = result.resultat.listes;
+//        var sorted_listes = listes.sort(function (a,b) {
+//                                            return b.vote - a.vote;
+//                                          }
+//                                       );
+//        assert.deepEqual(sorted_listes, listes);
+//    })
+//    .expect('should provide the expected response', function (err, res, body) {
+//        assert.deepEqual(expectedResultCirconscription, JSON.parse(body));
+//    })
     .export(module);
