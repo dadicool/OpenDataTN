9e0b1244d9fa919bfa8214382d705c2603c026ab
Dali Kilani <dali.kilani@gmail.com>
2012-01-03 20:45:59 -0800

    More fixes for the aggregation endpoints

--- tnac_api/test/tnac-aggregation-test.js
+++ tnac_api/test/tnac-aggregation-test.js
@@ -74,34 +74,34 @@ suite.use(config.host, config.port)
     .expect('should provide the expected response', function (err, res, body) {
         assert.deepEqual(expectedResultDelegation, JSON.parse(body));
     })
-//    .undiscuss()
-//    .unpath()
-//    .discuss('When asking for the aggregated results for a specific circonscription')
-//    .path('/tnac/v1/vote')
-//    .get('/'+ circ_test_id )
-//    .expect(200)
-//    .expect('should respond with exactly one circonscription', function (err, res, body) {
-//        assert.isObject(JSON.parse(body));
-//    })
-//    .expect('should have a "resultat" with at least votes for one list ', function (err, res, body) {
-//        var result = JSON.parse(body);
-//        assert.isDefined(result.resultat);
-//        assert.isArray(result.resultat.listes);
-//        assert.isTrue(result.resultat.listes.length >= 1);
-//    })
-//    .expect('should return the votes for the list in descending order', function (err, res, body) {
-//        var result = JSON.parse(body);
-//        assert.isDefined(result.resultat);
-//        assert.isArray(result.resultat.listes);
-//        assert.isTrue(result.resultat.listes.length >= 1);
-//        var listes = result.resultat.listes;
-//        var sorted_listes = listes.sort(function (a,b) {
-//                                            return b.vote - a.vote;
-//                                          }
-//                                       );
-//        assert.deepEqual(sorted_listes, listes);
-//    })
-//    .expect('should provide the expected response', function (err, res, body) {
-//        assert.deepEqual(expectedResultCirconscription, JSON.parse(body));
-//    })
+    .undiscuss()
+    .unpath()
+    .discuss('When asking for the aggregated results for a specific circonscription')
+    .path('/tnac/v1/vote')
+    .get('/'+ circ_test_id )
+    .expect(200)
+    .expect('should respond with exactly one circonscription', function (err, res, body) {
+        assert.isObject(JSON.parse(body));
+    })
+    .expect('should have a "resultat" with at least votes for one list ', function (err, res, body) {
+        var result = JSON.parse(body);
+        assert.isDefined(result.resultat);
+        assert.isArray(result.resultat.listes);
+        assert.isTrue(result.resultat.listes.length >= 1);
+    })
+    .expect('should return the votes for the list in descending order', function (err, res, body) {
+        var result = JSON.parse(body);
+        assert.isDefined(result.resultat);
+        assert.isArray(result.resultat.listes);
+        assert.isTrue(result.resultat.listes.length >= 1);
+        var listes = result.resultat.listes;
+        var sorted_listes = listes.sort(function (a,b) {
+                                            return b.vote - a.vote;
+                                          }
+                                       );
+        assert.deepEqual(sorted_listes, listes);
+    })
+    .expect('should provide the expected response', function (err, res, body) {
+        assert.deepEqual(expectedResultCirconscription, JSON.parse(body));
+    })
     .export(module);
